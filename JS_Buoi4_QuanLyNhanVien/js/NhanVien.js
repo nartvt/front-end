1dfc8a69361ba2960ed41b2f63580d846c697343
tranvantai0011 <tooyoutooyou@gmail.com>
2019-05-26 22:19:20 +0700

    add assets

--- JS_Buoi4_QuanLyNhanVien/js/NhanVien.js
+++
@@ -1,8 +0,0 @@
-function NhanVien(_maNV, _tenNV, _email, _matKhau, _date, _chucVu) {
-    this.maNhanVien = _maNV;
-    this.tenNhanVien = _tenNV;
-    this.email = _email;
-    this.matKhau = _matKhau;
-    this.date = _date;
-    this.chucVu = _chucVu;
-}
